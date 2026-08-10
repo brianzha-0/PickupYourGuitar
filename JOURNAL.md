bb84a4ededfeffb2a655c0c3dfff508dcd9691a0
BrianZhao10 <brianzhaoptimal@gmail.com>
2026-08-10 14:13:50 -0400

    All

--- JOURNAL.md
+++ JOURNAL.md
@@ -6,10 +6,10 @@ Rudimentary Deployment:
 Open index.html inside src which is at the root of the project folder using Live Server—an installable .vscode extension, through running the command shortcuts Alt+L followed by Alt+O
 
 ProgreSKL:
-![Turn Over](/asset/eTimeOlogY/adjournIEÉE.png)
-![Yielded Final Result](/asset/eTimeOlogY/flIPADdress.png)
+![Turn Over](/src/asset/eTimeOlogY/adjournIEÉE.png)
+![Yielded Final Result](/src/asset/eTimeOlogY/flIPADdress.png)
 
 (W)all (Challenges) I ran into:
-![Ran into an otherwise tedious and bothersome error where the camera and/or microphone permission settings are overrided by system; on a different network (like the school district one for summer school), I was able to run the app smoothly. At that time, I could record and stop recording videos and also see a live feed when not yet capturing anything and download the .webm file of that playback!](/asset/eTimeOlogY/Trubble.png)
-![I had fun with the capabilities of the implemented aspect of the my laptop camera. . . Interesting Fact: Laptops and guitar both belong on the lap. Both relate to strings as well!](/asset/eTimeOlogY/ThroughTheProof.png)
-![Render the ability for users to hoist and host their personal portable document files (Pdfs) in a straightforward and legible manner](/asset/eTimeOlogY/emplorer.png)
+![Ran into an otherwise tedious and bothersome error where the camera and/or microphone permission settings are overrided by system; on a different network (like the school district one for summer school), I was able to run the app smoothly. At that time, I could record and stop recording videos and also see a live feed when not yet capturing anything and download the .webm file of that playback!](/src/asset/eTimeOlogY/Trubble.png)
+![I had fun with the capabilities of the implemented aspect of the my laptop camera. . . Interesting Fact: Laptops and guitar both belong on the lap. Both relate to strings as well!](/src/asset/eTimeOlogY/ThroughTheProof.png)
+![Render the ability for users to hoist and host their personal portable document files (Pdfs) in a straightforward and legible manner](/src/asset/eTimeOlogY/emplorer.png)
--- src/api.js
+++ src/api.js
@@ -19,127 +19,11 @@ const prevBtn = document.getElementById("prev");
 const defaultPdf = "song1.pdf";
 
 let pages = [];
-let currentPage = 0;
+let currentPage = 0, defaultLoaded = true;
 
 async function loadPdf(source) {
 
-pages = [];
-currentPage = 0;
-
-const pdf = await pdfjsLib.getDocument(source).promise;
-
-for (let i = 1; i <= pdf.numPages; i++) {
-
-    const page = await pdf.getPage(i);
-
-    const viewport = page.getViewport({
-        scale: 1.5
-    });
-
-    const canvas = document.createElement("canvas");
-
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
-
-    await page.render({
-        canvasContext: canvas.getContext("2d"),
-        viewport
-    }).promise;
-
-    pages.push(canvas);
-}
-
-showPages();
-
-}
-
-function drawCanvas(source, target) {
-
-target.width = source.width;
-target.height = source.height;
-
-const ctx = target.getContext("2d");
-
-ctx.clearRect(0, 0, target.width, target.height);
-ctx.drawImage(source, 0, 0);
-
-}
-
-function showPages() {
-
-leftCtx.clearRect(
-    0,
-    0,
-    leftCanvas.width,
-    leftCanvas.height
-);
-
-rightCtx.clearRect(
-    0,
-    0,
-    rightCanvas.width,
-    rightCanvas.height
-);
-
-if (pages[currentPage]) {
-
-    drawCanvas(
-        pages[currentPage],
-        leftCanvas
-    );
-
-}
-
-if (pages[currentPage + 1]) {
-
-    drawCanvas(
-        pages[currentPage + 1],
-        rightCanvas
-    );
-
-}
-
-}
-
-fileName.textContent = "song1.pdf";
-
-leftCtx.clearRect(
-0,
-0,
-leftCanvas.width,
-leftCanvas.height
-);
-
-rightCtx.clearRect(
-0,
-0,
-rightCanvas.width,
-rightCanvas.height
-);
-
-loadPdf(defaultPdf);
-
-uploadInput.addEventListener("change", async (e) => {
-
-const selectedFiles = [...e.target.files];
-
-if (selectedFiles.length === 0) {
-    return;
-}
-
-pages = [];
-currentPage = 0;
-
-fileName.textContent =
-    `${selectedFiles.length} PDF${selectedFiles.length > 1 ? "s" : ""} Loaded`;
-
-for (const file of selectedFiles) {
-
-    const buffer = await file.arrayBuffer();
-
-    const pdf = await pdfjsLib.getDocument({
-        data: buffer
-    }).promise;
+    const pdf = await pdfjsLib.getDocument(source).promise;
 
     for (let i = 1; i <= pdf.numPages; i++) {
 
@@ -163,67 +47,188 @@ for (const file of selectedFiles) {
 
     }
 
+    showPages();
+
 }
 
-showPages();
+function drawCanvas(source, target) {
+
+    target.width = source.width;
+    target.height = source.height;
+
+    const ctx = target.getContext("2d");
+
+    ctx.clearRect(0, 0, target.width, target.height);
+    ctx.drawImage(source, 0, 0);
+
+}
+
+function showPages() {
+
+    leftCtx.clearRect(
+        0,
+        0,
+        leftCanvas.width,
+        leftCanvas.height
+    );
+
+    rightCtx.clearRect(
+        0,
+        0,
+        rightCanvas.width,
+        rightCanvas.height
+    );
+
+    if (pages[currentPage]) {
+
+        drawCanvas(
+            pages[currentPage],
+            leftCanvas
+        );
+
+    }
+
+    if (pages[currentPage + 1]) {
+
+        drawCanvas(
+            pages[currentPage + 1],
+            rightCanvas
+        );
+
+    }
+
+}
+
+fileName.textContent = "song1.pdf";
+
+leftCtx.clearRect(
+    0,
+    0,
+    leftCanvas.width,
+    leftCanvas.height
+);
+
+rightCtx.clearRect(
+    0,
+    0,
+    rightCanvas.width,
+    rightCanvas.height
+);
+
+loadPdf(defaultPdf);
+
+uploadInput.addEventListener("change", async (e) => {
+
+    const selectedFiles = [...e.target.files];
+
+    if (selectedFiles.length === 0)
+
+        return;
+
+    
+
+    if (defaultLoaded) {
+
+        pages = [];
+        currentPage = 0;
+        defaultLoaded = false;
+
+    }
+
+    for (const file of selectedFiles) {
+
+        const buffer = await file.arrayBuffer();
+
+        const pdf = await pdfjsLib.getDocument({
+            data: buffer
+        }).promise;
+
+        for (let i = 1; i <= pdf.numPages; i++) {
+
+            const page = await pdf.getPage(i);
+
+            const viewport = page.getViewport({
+                scale: 1.5
+            });
+
+            const canvas = document.createElement("canvas");
+
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+
+            await page.render({
+                canvasContext: canvas.getContext("2d"),
+                viewport
+            }).promise;
+
+            pages.push(canvas);
+
+        }
+
+    }
+
+    fileName.textContent =
+        `${pages.length} Pages Loaded`;
+
+    showPages();
 
 });
 
 nextBtn.addEventListener("click", () => {
 
-if (currentPage + 2 < pages.length) {
+    if (currentPage + 2 < pages.length) {
 
-    const right =
-        document.querySelector(".right");
+        const right =
+            document.querySelector(".right");
 
-    currentPage += 2;
+        currentPage += 2;
 
-    drawCanvas(
-        pages[currentPage],
-        rightCanvas
-    );
+        drawCanvas(
+            pages[currentPage],
+            rightCanvas
+        );
 
-    right.classList.add("turn");
+        right.classList.add("turn");
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-        showPages();
+            showPages();
 
-        right.classList.remove("turn");
+            right.classList.remove("turn");
 
-    }, 400);
+        }, 400);
 
-}
+    }
 
 });
 
 prevBtn.addEventListener("click", () => {
 
-if (currentPage >= 2) {
+    if (currentPage >= 2) {
 
-    const left =
-        document.querySelector(".left");
+        const left =
+            document.querySelector(".left");
 
-    currentPage -= 2;
+        currentPage -= 2;
 
-    showPages();
+        showPages();
 
-    left.classList.add("turn-back");
-
-    document
-        .querySelector(".right")
-        .classList.add("shift-back");
-
-    setTimeout(() => {
-
-        left.classList.remove("turn-back");
+        left.classList.add("turn-back");
 
         document
             .querySelector(".right")
-            .classList.remove("shift-back");
+            .classList.add("shift-back");
 
-    }, 400);
+        setTimeout(() => {
 
-}
+            left.classList.remove("turn-back");
+
+            document
+                .querySelector(".right")
+                .classList.remove("shift-back");
+
+        }, 400);
+
+    }
 
 });
